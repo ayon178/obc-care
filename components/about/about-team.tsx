cf9a78ed4c0250ab0c6ad3b8c7e44b284d76898d
AyonJD <ayonjodder177@gmail.com>
2026-01-03 12:17:16 +0600

    feat: add AboutTeam component to display team members with their roles and contact information.

--- components/about/about-team.tsx
+++ components/about/about-team.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import TeamCard from "./team-card"
-import { Truck, Package, User, HardHat } from "lucide-react"
+import { Briefcase, Calculator, Settings, ClipboardList } from "lucide-react"
 import { useTranslations } from "next-intl"
 
 export default function AboutTeam() {
@@ -12,7 +12,7 @@ export default function AboutTeam() {
       name: "Eugen Reimer",
       role: t("roles.0"),
       image: "/about/team/eugen.png",
-      icon: <Truck className="h-8 w-8" />,
+      icon: <Briefcase className="h-8 w-8" />,
       contact: "+49 176 45922485",
       socials: {
         linkedin: "https://www.linkedin.com/in/timecriticallogistics-eugene-reimer-obccare/",
@@ -24,7 +24,7 @@ export default function AboutTeam() {
       name: "Tatiana Reimer",
       role: t("roles.1"),
       image: "/about/team/Tanya.png",
-      icon: <Package className="h-8 w-8" />,
+      icon: <Calculator className="h-8 w-8" />,
       contact: "+49 179 1394318",
       socials: {
         linkedin: "https://www.linkedin.com/in/tatiana-reimer-053431288/",
@@ -35,7 +35,7 @@ export default function AboutTeam() {
       name: "Alexander Reimer",
       role: t("roles.2"),
       image: "/about/team/alexander.png",
-      icon: <User className="h-8 w-8" />,
+      icon: <Settings className="h-8 w-8" />,
       contact: "+44 7488 818707",
       socials: {
         linkedin: "https://www.linkedin.com/in/alexanderreimer/",
@@ -46,21 +46,21 @@ export default function AboutTeam() {
       name: "Cristine Acosta",
       role: t("roles.3"),
       image: "/about/team/cristine.png",
-      icon: <HardHat className="h-8 w-8" />,
+      icon: <ClipboardList className="h-8 w-8" />,
       contact: "+1 (610) 245-8815",
     },
     {
       name: "Noreleen Neypes",
       role: t("roles.4"),
       image: "/about/team/noreleen.jpeg", // Reusing image for now
-      icon: <Package className="h-8 w-8" />,
+      icon: <ClipboardList className="h-8 w-8" />,
       contact: "+44 7700 183290",
     },
     {
       name: "Renz Abad",
       role: t("roles.5"),
       image: "/about/team/renz.png", // Reusing image for now
-      icon: <Truck className="h-8 w-8" />,
+      icon: <ClipboardList className="h-8 w-8" />,
       contact: "+44 7700 183458",
     },
   ]
